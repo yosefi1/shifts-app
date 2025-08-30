73e3811f2f06a8a52e419b30f797dca8ce4cb8f1
yosefi1 <sdoko2@gmail.com>
2025-08-30 17:38:34 +0300

    Test: add basic React Router to isolate routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Shifts App - Test Page</h1>
-      <p>If you can see this, basic React is working!</p>
-      <p>No routing - just a simple div</p>
-      <p>Firebase status: Loading...</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <div style={{ padding: '20px', textAlign: 'center' }}>
+            <h1>Shifts App - Test Page</h1>
+            <p>If you can see this, basic React is working!</p>
+            <p>Basic routing test</p>
+            <p>Firebase status: Loading...</p>
+          </div>
+        } />
+      </Routes>
+    </Router>
   )
 }
 
